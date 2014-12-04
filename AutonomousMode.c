e5331627e0eb567dc9c05cd314a721f05ff934ef
dan123222123 <dan134535334@gmail.com>
2014-12-04 16:56:44 -0500

    Additions to AutonomousMode.c

--- AutonomousMode.c
+++ AutonomousMode.c
@@ -4,6 +4,8 @@
 #pragma config(Sensor, S4,     ir,             sensorHiTechnicIRSeeker1200)
 #pragma config(Motor,  mtr_S1_C1_1,     rightMotor,    tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     leftMotor,     tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S2_C1_1,     winchMotor,     tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S2_C1_2,     conveyerMotor,     tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C2_1,    servo1,               tServoNone)
 #pragma config(Servo,  srvo_S1_C2_2,    servo2,               tServoNone)
 #pragma config(Servo,  srvo_S1_C2_3,    servo3,               tServoNone)
@@ -16,24 +18,31 @@
 #include "hitechnic-gyro.h"
 
 task initializeRobot(){
-	nMotorEncoder = 0;
-	nMotorEncoder = 0;
+	nMotorEncoder[rightMotor] = 0;
+	nMotorEncoder[leftMotor] = 0;
+	nMotorEncoder[winchMotor] = 0;
+	while(nMotorEncoder[winchMotor]<(1440*3){
+		motor[winchMotor] = 100;		
+	}
+	nMotorEncoder[winchMotor] = 0;
 	ReadSensorHTAccel(S1, x, y, z);
+	HTGYROstartCal(tSensors link);
 }
 HTIRS2setDSPMode(1,-DSP_1200);
-
+int gyroThreshold = -3;
 task main(){
 	initializeRobot;
 	
 	while(true){
 		int IRSensorDir = IRHTIRS2readACDir(1);
+		int gyroSensorValue = HTGYROreadRot(S2);
 		//Displays the Encoder Values + IR Sensor Direction on the NXT's screen
 		nxtDisplayTextLine(1,"%d",nMotorEncoder[rightMotor]);
 		nxtDisplayTextLine(2,"%d",nMotorEncoder[leftMotor]);
 		nxtDisplayTextLine(3,"%d",IRSensorDir);
 		//Reseting Motor Encoders	
 		nMotorEncoder[rightMotor] = 0;
-		nMotorEncoder[leftMotor] = 0;
+		nMotorEncoder[leftMotor] = 0;	
 		//First Robot Movement - Down the ramp
 		while(nMotorEncoder[rightMotor]<(1440)){
 			motor[rightMotor] = 100;
@@ -48,7 +57,7 @@ task main(){
 			motor[leftMotor] = -50;
 		}
 		//Robot Third Movement - IRSensor fine adjustments
-		while(SensorValue[IR1] != 5){
+		while(SensorValue[IR1] != 4){
 		IR1value = SensorValue[IR1];
 		nxtDisplayCenteredTextLine(5,"%d",IR1value);
 		motor[rightMotor] = 20;
@@ -58,9 +67,32 @@ task main(){
 		nMotorEncoder[rightMotor] = 0;
 		nMotorEncoder[leftMotor] = 0;
 		//Robot Fourth Movement - Movement towards central dispenser
-		while(nMotorEncoder[rightMotor]<(1440*5)){
+		while(nMotorEncoder[rightMotor]<(1440*3)){
 			motor[rightMotor] = 100;
 			motor[leftMotor] = 100;
+		}
+		//Resetting Motor Encoders
+		nMotorEncoder[rightMotor] = 0;
+		nMotorEncoder[leftMotor] = 0;
+		//Robot Fourth Movement cont. - Precision movement towards central dispenser
+		while(nMotorEncoder[rightMotor]<(1440*2)){
+			motor[rightMotor] = 25;
+			motor[leftMotor] = 25;
+		}
+		//Resetting motor encoders
+		nMotorEncoder[leftMotor] = 0;
+		nMotorEncoder[rightMotor] = 0;
+		//Robot Fifth Movement - Backtracking movements to be in a position to readjust heading
+		while(nMotorEncoder[rightMotor]<(-1440*2)){
+			motor[rightMotor] = -100;
+			motor[leftMotor] = -100;
+		}
+		//Robot Sxth Movement - IRSensor readjustment to place ball in recepticle
+		while(SensorValue[IR1] != 5){
+		IR1value = SensorValue[IR1];
+		nxtDisplayCenteredTextLine(5,"%d",IR1value);
+		motor[rightMotor] = 20;
+		motor[rightMotor] = -20;
 		}
 		//Spin in place / placeholder
 		motor[rightMotor] = 50;
